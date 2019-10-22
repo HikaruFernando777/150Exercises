87d585b708b504dc9b3733d2eaf409e1460352aa
Laura Velez <laura.velez@globant.com>
2019-10-22 09:38:11 -0500

    First Basic

---
+++ src/Example/BS01-10/E01_Hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace src
+{
+    public class E01_Hello
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Lau");
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            E01_Hello e01 = new E01_Hello();
+            e01.Print();
+        }
+    }
+}
