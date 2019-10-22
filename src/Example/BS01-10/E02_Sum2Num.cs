8952d225ad51a5683af85dd6bfd4aaa6cf0a9763
Laura Velez <laura.velez@globant.com>
2019-10-22 12:56:39 -0500

    Added new exercise02

---
+++ src/Example/BS01-10/E02_Sum2Num.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace src
+{
+    public class E02_Sum2Num
+    {
+        public void Sum(int numA, int numB)
+        {
+            var sum = 0;
+            sum = numA + numB;
+            Console.WriteLine(sum);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -6,8 +6,12 @@ namespace src
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.WriteLine("E1: ");
             E01_Hello e01 = new E01_Hello();
             e01.Print();
+            // Console.WriteLine();
+            // Console.WriteLine("E2: ");
         }
     }
 }
