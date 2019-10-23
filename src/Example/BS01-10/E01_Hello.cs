da0c67d13f34bffd2e138f686081623f32e8801d
Laura Velez <laura.velez@globant.com>
2019-10-23 16:51:57 -0500

    Created class division

--- src/Example/BS01-10/E01_Hello.cs
+++ src/Example/BS01-10/E01_Hello.cs
@@ -4,6 +4,12 @@ namespace src
 {
     public static class E01_Hello
     {
+        /*
+            Si un método (una función) está precedido por la palabra "static", 
+            indica que es un "método de clase", es decir, un método que se podría 
+            usar sin necesidad de declarar ningún objeto de la clase.
+            
+         */
         public static void firstE(){
             Console.WriteLine();
             Console.WriteLine("Ex1: ");
